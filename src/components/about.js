ce273e65ab227a1e3d56da15377df6b86cd9587c
Caleb Kress <calebkress@gmail.com>
2018-06-26 00:20:25 -0500

    started "about" branch

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class About extends Component {
   render () {
     return (
-      <p>in about me</p>
+      <p>Editing this text so I can start the about branch.</p>
     )
   }
 }
